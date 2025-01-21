96acb3cc1340f93970312cd8df2290c2084fb23f
Likhith-18 <vparvathi222@gmail.com>
2025-01-21 18:54:05 +0530

    Remove reload option from uvicorn run command in app.py

--- app.py
+++ app.py
@@ -27,4 +27,4 @@ async def get_details():
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=8000)
